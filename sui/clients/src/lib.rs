1c84a861c884aa6044f32b6872cb5691671ca57e
songxuexian <songxuexian@hnnewsoft.com>
2022-09-05 16:45:34 +0800

    finish sui client send tx

--- sui/clients/src/lib.rs
+++ sui/clients/src/lib.rs
@@ -1,23 +1,28 @@
-use sui_sdk::crypto::{SuiKeystore, KeystoreType};
+use std::{fs::File, io::BufReader};
+use sui_sdk::{
+    crypto::{KeystoreType, SuiKeystore},
+    types::crypto::SignatureScheme,
+};
 
 pub mod address;
 pub mod transactions;
 pub mod ws;
 
-
 pub fn local_keystore() -> Result<SuiKeystore, anyhow::Error> {
     // Load keystore from ~/.sui/sui_config/sui.keystore
     let keystore_path = match dirs::home_dir() {
-        Some(v) => v.join(".sui").join("sui_config").join("5ac37128a02c0e83360e315c01a50103b3bd212c26f96818a0748e27a4159de0.key"),
+        Some(v) => v.join(".sui").join("sui_config").join("sui.keystore"),
         None => panic!("Cannot obtain home directory path"),
     };
 
+    // let reader = BufReader::new(File::open(keystore_path)?);
+    // let kp_strings: Vec<String> = serde_json::from_reader(reader)?;
+    // println!("{:?}", kp_strings);
     let keystore = KeystoreType::File(keystore_path).init()?;
+    let addresses = keystore.addresses();
+    println!("{:?}", addresses);
     Ok(keystore)
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+    // Ok(())
 }
 
 #[cfg(test)]
@@ -25,14 +30,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-    #[test]
     fn load_keystore_test() {
         let _result = local_keystore().unwrap();
         // println!("{:#?}",result)
     }
-
 }
--- sui/clients/src/transactions.rs
+++ sui/clients/src/transactions.rs
@@ -4,7 +4,7 @@ use sui_sdk::{
     types::{
         base_types::{ObjectID, SuiAddress},
         crypto::Signature,
-        messages::{Transaction, TransactionData},
+        messages::Transaction,
     },
     SuiClient,
 };
@@ -12,22 +12,14 @@ use sui_sdk::{
 pub async fn default_conduct_transaction(sui: &SuiClient) -> Result<(), anyhow::Error> {
     // Load keystore from ~/.sui/sui_config/sui.keystore
     let keystore_path = match dirs::home_dir() {
-        Some(v) => v
-            .join(".sui")
-            .join("sui_config")
-            .join("5ac37128a02c0e83360e315c01a50103b3bd212c26f96818a0748e27a4159de0.key"),
+        Some(v) => v.join(".sui").join("sui_config").join("sui.keystore"),
         None => panic!("Cannot obtain home directory path"),
     };
 
-    let my_address = SuiAddress::from_str("0x5ac37128a02c0e83360e315c01a50103b3bd212c")?;
-    let gas_object_id = ObjectID::from_str("0x09aa590503d408433e433b15a70133d4e3e9fa2e")?;
-    let recipient = SuiAddress::from_str("0x20ef8251e36d6247b372975c374914f2e2b16120")?;
+    let my_address = SuiAddress::from_str("0x054d99f30cdbaea6dd5981d4b8673f4c6fb3b9b0")?;
+    let gas_object_id = ObjectID::from_str("0x597f6133abacc63f255513ce189761d9e0f2fae2")?;
+    let recipient = SuiAddress::from_str("0xe806e842ca27361f74983711bcb8d9e6612fa06d")?;
 
-    //     let object = sui.read_api().get_parsed_object(gas_object_id).await?;
-    //     let object = object.into_object()?.reference.to_object_ref();
-    //     let transfer_tx = TransactionData::new_transfer_sui(
-    //         recipient, my_address, Some(1000), object, 1000,
-    //    );
     // Create a sui transfer transaction
     let transfer_tx = sui
         .transaction_builder()
@@ -36,8 +28,6 @@ pub async fn default_conduct_transaction(sui: &SuiClient) -> Result<(), anyhow::
 
     // Get signer from keystore
     let keystore = KeystoreType::File(keystore_path).init()?;
-    let addresses = keystore.addresses();
-    println!("{:?}", addresses);
     let signer = keystore.signer(my_address);
 
     // Sign the transaction

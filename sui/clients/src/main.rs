4a461c5ed738eca758eaa7c328aaf2db47423443
songxuexian <songxuexian@hnnewsoft.com>
2022-09-06 17:50:39 +0800

    split sui tx

--- sui/clients/src/main.rs
+++ sui/clients/src/main.rs
@@ -16,8 +16,10 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let sui = SuiClient::new_rpc_client("https://gateway.devnet.sui.io:443", None).await?;
     // address::default_get_objects_owned_by_address().await
-    transactions::default_conduct_transaction(&sui).await
-    // let query_obj_id = "0x258eceba1168d876a55267b79cb36165f6f90c55";
+    let from = "0x054d99f30cdbaea6dd5981d4b8673f4c6fb3b9b0";
+    let to = "0xe806e842ca27361f74983711bcb8d9e6612fa06d";
+    let gas = "0x597f6133abacc63f255513ce189761d9e0f2fae2";
+    transactions::default_conduct_transaction(&sui, form, to, gas).await
     // address::default_get_object_by_id(&sui, query_obj_id).await;
     // address::default_get_raw_object_by_id(&sui, query_obj_id).await
 }
--- sui/clients/src/transactions.rs
+++ sui/clients/src/transactions.rs
@@ -4,34 +4,22 @@ use sui_sdk::{
     types::{
         base_types::{ObjectID, SuiAddress},
         crypto::Signature,
-        messages::Transaction,
+        messages::{Transaction, TransactionData},
     },
     SuiClient,
 };
 
-pub async fn default_conduct_transaction(sui: &SuiClient) -> Result<(), anyhow::Error> {
-    // Load keystore from ~/.sui/sui_config/sui.keystore
-    let keystore_path = match dirs::home_dir() {
-        Some(v) => v.join(".sui").join("sui_config").join("sui.keystore"),
-        None => panic!("Cannot obtain home directory path"),
-    };
-
-    let my_address = SuiAddress::from_str("0x054d99f30cdbaea6dd5981d4b8673f4c6fb3b9b0")?;
-    let gas_object_id = ObjectID::from_str("0x597f6133abacc63f255513ce189761d9e0f2fae2")?;
-    let recipient = SuiAddress::from_str("0xe806e842ca27361f74983711bcb8d9e6612fa06d")?;
-
+pub async fn default_conduct_transaction(
+    sui: &SuiClient,
+    from: &str,
+    to: &str,
+    gas: &str,
+) -> Result<(), anyhow::Error> {
     // Create a sui transfer transaction
-    let transfer_tx = sui
-        .transaction_builder()
-        .transfer_sui(my_address, gas_object_id, 1000, recipient, Some(1000))
-        .await?;
-
-    // Get signer from keystore
-    let keystore = KeystoreType::File(keystore_path).init()?;
-    let signer = keystore.signer(my_address);
+    let transfer_tx = build_transaction_sui(sui, from, to, gas).await?;
 
     // Sign the transaction
-    let signature = Signature::new(&transfer_tx, &signer);
+    let signature = sign_transaction(from, &transfer_tx);
 
     // Execute the transaction
     let transaction_response = sui
@@ -42,4 +30,46 @@ pub async fn default_conduct_transaction(sui: &SuiClient) -> Result<(), anyhow::
     println!("{:?}", transaction_response);
 
     Ok(())
+}
+
+// why build transaction need client?
+pub async fn build_transaction_sui(
+    sui: &SuiClient,
+    from: &str,
+    to: &str,
+    gas: &str,
+) -> Result<TransactionData, anyhow::Error> {
+    let from = SuiAddress::from_str(from)?;
+    let gas_object_id = ObjectID::from_str(gas)?;
+    let recipient = SuiAddress::from_str(to)?;
+
+    // Create a sui transfer transaction
+    let transfer_tx = sui
+        .transaction_builder()
+        .transfer_sui(from, gas_object_id, 1000, recipient, Some(1000))
+        .await?;
+
+    Ok(transfer_tx)
+}
+
+// why build transaction need client?
+pub async fn sign_transaction(
+    from: &str,
+    data: &TransactionData,
+) -> Result<Signature, anyhow::Error> {
+    // Load keystore from ~/.sui/sui_config/sui.keystore
+    let keystore_path = match dirs::home_dir() {
+        Some(v) => v.join(".sui").join("sui_config").join("sui.keystore"),
+        None => panic!("Cannot obtain home directory path"),
+    };
+    let from = SuiAddress::from_str(from)?;
+
+    // Get signer from keystore
+    let keystore = KeystoreType::File(keystore_path).init()?;
+    let signer = keystore.signer(from);
+
+    // Sign the transaction
+    let signature = Signature::new(data, &signer);
+
+    Ok(signature)
 }
